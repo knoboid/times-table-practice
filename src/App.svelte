12f241da585e1adf0cf4b6bc527c67b137699dd2
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-01-22 11:11:34 +0000

    Extracted Multiplication component
    Added num1 and num2 props

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,7 @@
-<main class="h-64 flex flex-col justify-center">
-  <div class="multiplication">
-    <div class="problem">2&times;2=</div>
-    <input
-      class="answer"
-      type="number"
-      maxlength="2"
-      size="2"
-      placeholder="?"
-    />
-  </div>
-</main>
+<script>
+  import Multiplication from "./lib/Multiplication.svelte";
+</script>
 
-<style>
-  .problem {
-    font-size: 9em;
-    font-weight: 100;
-  }
-  .answer {
-    font-size: 8em;
-    font-weight: 150;
-    filter: drop-shadow(2px 2px 5px #aaa);
-    border-radius: 0.25em;
-    text-align: center;
-    width: 2em;
-    caret-color: #bbb;
-  }
-  .multiplication {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  }
-</style>
+<main class="h-64 flex flex-col justify-center">
+  <Multiplication num1="3" num2="5" />
+</main>
---
+++ src/lib/Multiplication.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let num1;
+  export let num2;
+</script>
+
+<div class="multiplication">
+  <div class="problem">{num1}&times;{num2}=</div>
+  <input class="answer" type="number" maxlength="2" size="2" placeholder="?" />
+</div>
+
+<style>
+  .problem {
+    font-size: 9em;
+    font-weight: 100;
+  }
+  .answer {
+    font-size: 8em;
+    font-weight: 150;
+    filter: drop-shadow(2px 2px 5px #aaa);
+    border-radius: 0.25em;
+    text-align: center;
+    width: 2em;
+    caret-color: #bbb;
+  }
+  .multiplication {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+</style>
