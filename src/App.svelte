e65f7fc96f2ed7c2f26c9c0d2c1ddd8dc37d97cf
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-01-22 11:40:59 +0000

    Used if block to show question or answer Used if block to show question or answer depending on state.

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,17 @@
   let num1 = 4;
   let num2 = 6;
 
+  const showingQuestion = "SHOWING QUESTION";
+  const showingAnswer = "SHOWING ANSWER";
+
+  let appState = showingQuestion;
+
   function handleKeypress(e) {
     if (e.key === "Enter") {
       let usersAnswer = e.target.value;
       e.target.value = "";
       const answer = num1 * num2;
+      appState = showingAnswer;
       if (usersAnswer == answer) {
         console.log("CORRECT");
       } else {
@@ -19,5 +25,9 @@
 </script>
 
 <main class="h-64 flex flex-col justify-center">
-  <Multiplication {num1} {num2} on:keypress={handleKeypress} />
+  {#if appState === showingQuestion}
+    <Multiplication {num1} {num2} on:keypress={handleKeypress} />
+  {:else if appState === showingAnswer}
+    <div>Showing Answer</div>
+  {/if}
 </main>
